ceb4a2d665cd8c534e5ff064cf0f5b90914de4d4
Egor <belyaeff.egor2017@yandex.ru>
2024-06-26 22:13:35 +0300

    new func

--- sqrt_newton.rkt
+++ sqrt_newton.rkt
@@ -23,8 +23,30 @@
   (* x x))
 
 
+ (define (good-enough-new? g-last g-old)
+          (< (/ (* (abs (- g-last g-old)) 100)
+                   g-last)
+             0.01))
+
+
+(define (sqrt-iter-new guess guess-old x)
+  (if
+   (good-enough-new? guess guess-old)
+   guess
+   (sqrt-iter-new (improve guess x) guess x)))
+
+(define (sqrt-new x)
+  (sqrt-iter-new 1.0 0.0 x))
+
+
 (display (sqrt 4))
+(newline)
+(display (sqrt-new 4))
 (newline)
 (display (sqrt 16))
 (newline)
+(display (sqrt-new 16))
+(newline)
 (display (sqrt 100))
+(newline)
+(display (sqrt-new 100))
